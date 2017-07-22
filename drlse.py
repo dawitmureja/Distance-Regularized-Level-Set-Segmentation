ebf9c171522a541e483262f4cce62dc92c3210e9
Dawitmu <dawitmureja@kaist.ac.kr>
2017-07-22 14:41:49 +0900

    Add files via upload

---
+++ drlse.py
@@ -0,0 +1,115 @@
+import numpy as np
+import ipdb
+def del2(M):
+    r,c = M.shape
+    D = np.zeros(M.shape)
+    for i in range(r):
+        for j in range(c):
+            if (i == 0 and j == 0):
+                D[i,j] = -5 * M[i,j+1] + 4*M[i,j + 2] - M[i,j+3] - 5*M[i+1,j] + 4*M[i+2,j]-M[i+3,j] + 4 *M[i,j]
+            elif (i == 0 and j == c-1):
+                D[i,j] = -5 * M[i,j-1] + 4*M[i,j - 2] - M[i,j-3] - 5*M[i+1,j] + 4*M[i+2,j]-M[i+3,j] + 4*M[i,j]
+            elif(i == r-1 and j == 0):
+                D[i,j] = -5 * M[i,j+1] + 4*M[i,j+2] - M[i,j+3] - 5*M[i-1,j] + 4*M[i-2,j]-M[i-3,j] + 4*M[i,j]
+            elif (i == r-1 and j == c-1):
+                D[i,j] = -5 * M[i,j-1] + 4*M[i,j - 2] - M[i,j-3] - 5*M[i-1,j] + 4*M[i-2,j]-M[i-3,j] + 4*M[i,j]
+            elif (i == r-1):
+                D[i,j] = -5 * M[i-1,j] + 4*M[i-2,j] - M[i-3,j] + M[i,j+1] + M[i,j-1]
+            elif(i == 0):
+                D[i,j] = -5 * M[i+1,j] + 4*M[i+2,j] - M[i+3,j] + M[i,j+1] + M[i,j-1]
+            elif(j == 0):
+                D[i,j] = -5 * M[i,j+1] + 4*M[i,j+2] - M[i,j+3] + M[i+1,j] + M[i-1,j]
+            elif (j == c-1):
+                D[i,j] = -5 * M[i,j-1] + 4*M[i,j-2] - M[i,j-3] + M[i+1,j] + M[i-1,j]
+            else:
+                #D[i,j] = M[i+1,j] + M[i-1,j] -2*M[i,j]
+                D[i,j] = M[i+1,j] + M[i-1,j]+ M[i,j+1] + M[i, j-1] -4*M[i,j]
+
+    return D/4.0
+
+def gauss2D(shape=(3,3),sigma=0.5):
+
+    m,n = [(ss-1.)/2. for ss in shape]
+    y,x = np.ogrid[-m:m+1,-n:n+1]
+    h = np.exp( -(x*x + y*y) / (2.*sigma*sigma) )
+    h[ h < np.finfo(h.dtype).eps*h.max() ] = 0
+    sumh = h.sum()
+    if sumh != 0:
+        h /= sumh
+    return h
+
+def choose_boundary(a,b,c,d):
+    rows = np.array([a,b], dtype = np.int)
+    cols = np.array([c,d], dtype = np.int)
+    return [rows[:,np.newaxis],cols]
+
+def multiple_choose(a,b,c,d, m_row = False):
+    if m_row:
+        rows = np.array(range(a,b), dtype = np.int)
+        cols = np.array([c,d], dtype = np.int)
+        return [rows[:,np.newaxis],cols]
+    else:
+        rows = np.array([a,b], dtype = np.int)
+        cols = np.array(range(c,d), dtype = np.int)
+        return [rows[:,np.newaxis],cols]
+
+#fix it so that it can work for non square matrices
+def NeumannBoundaryCond(f):
+    #make a function statisfy Neumann boundary condition
+    nrow,ncol = f.shape
+    g = f
+    g[choose_boundary(0,nrow-1,0,ncol-1)] = g[choose_boundary(2,nrow-3,2,ncol-3)]
+    g[multiple_choose(0, nrow-1,1,ncol-1, m_row = False)] = g[multiple_choose(2,nrow-3,1, ncol-1,m_row = False)]
+    g[multiple_choose(1, nrow-1,0,ncol-1, m_row = True)] = g[multiple_choose(1,nrow-1,2, ncol-3,m_row = True)]
+    return g
+
+def Dirac(x,sigma):
+    f = (1/(2 * sigma)) * (1 + np.cos((np.pi * x)/sigma))
+    b = np.all([x <= sigma, x >= -sigma], axis = 0)
+    return f * b
+
+def div(nx,ny):
+    nxx,junk = np.gradient(nx)
+    junk,nyy = np.gradient(ny)
+    return nxx + nyy
+
+def distReg_p2(phi):
+    phi_x,phi_y = np.gradient(phi)
+    s = np.sqrt(np.square(phi_x) + np.square(phi_y))
+    a = np.all([s >= 0, s <= 1], axis = 0)
+    b = s > 1
+    ps = a * np.sin(2*np.pi*s)/ (2 * np.pi) + b * (s-1)
+    dps = ((ps != 0) * ps + (ps == 0))/((s != 0) * s + (s == 0))
+    f = div(dps * phi_x - phi_x, dps * phi_y -phi_y) + 4 * del2(phi)
+    return f
+
+def drlse_edge(phi_0,g,lamda,mu,alfa,epsilon, timestep, iteration, potentialFunction):
+    phi = phi_0
+    vx,vy = np.gradient(g)
+    for k in range(iteration):
+        phi = NeumannBoundaryCond(phi)
+        phi_x,phi_y = np.gradient(phi)
+        s = np.sqrt(np.square(phi_x) + np.square(phi_y))
+        smallNumber = 1e-10
+        Nx = phi_x / (s+smallNumber)
+        Ny = phi_y / (s+smallNumber)
+        curvature = div(Nx,Ny)
+        if (potentialFunction == 'single-well'):
+            distRegTerm = 4 * del2(phi) - curvature
+        elif(potentialFunction == 'double-well'):
+            distRegTerm = distReg_p2(phi)
+        else:
+            printf('Error: Wrong choice of potential function')
+        diracPhi = Dirac(phi,epsilon)
+        areaTerm= diracPhi * g
+        edgeTerm =  diracPhi * (vx * Nx + vy * Ny) + diracPhi * g * curvature
+        phi += timestep * (mu * distRegTerm + lamda * edgeTerm + alfa * areaTerm)
+    return phi
+
+
+#print(np.gradient(np.arange(12).reshape(3,4)))
+#print(Dirac(np.array([.1,.2,.3,.4]),0.5))
+#a = np.arange(25).reshape(5,5)
+#print(a)
+#b = NeumannBoundaryCond(a)
+#print(b)
---
+++ seg_demo.py
@@ -0,0 +1,89 @@
+import numpy as np
+from scipy.misc import imread, imshow, imresize,imsave
+import ipdb
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from scipy.signal import convolve2d as conv2d
+from drlse import gauss2D, drlse_edge,del2,distReg_p2,Dirac
+
+img = imread('gourd.jpg')
+#img1 = mpimg.imread('sample.jpg')
+#img1 = img1[:,:,0]
+#img = (imread('gourd.bmp')).astype(int)
+img = img[:,:,0]
+#parameter setting
+timestep = 1
+mu = 0.2/timestep
+iter_inner = 5
+iter_outer = 20
+lamda = 5
+alfa = -3
+#alfa  =1.5
+epsilon = 1.5
+sigma = 0.8
+#sigma = 1.5
+G = gauss2D((15,15),sigma)
+img_smooth = conv2d(img,G,'same')
+ix,iy = np.gradient(img_smooth)
+f = np.square(ix) + np.square(iy)
+g = 1.0/ (1+ f)
+
+#initialize LSF as binary step function
+c0=2;
+initialLSF = c0 * np.ones(img.shape)
+#generate the initial region R0 as two rectangles
+initialLSF[24:35, 19:25] = -c0
+initialLSF[24:35, 39:50] = -c0
+phi = initialLSF
+#imshow(phi)
+#ipdb.set_trace()
+#print(phi[24:35, 39:50])
+#phi0 = drlse_edge(phi, g, lamda, mu, alfa, lamda, epsilon, iter_inner, 'single-well')
+#ipdb.set_trace()
+
+#ax = plt.figure(1).gca(projection='3d')
+x_ = np.arange(img.shape[0])
+y_ = np.arange(img.shape[1])
+[x,y] = np.meshgrid(x_,y_)
+"""ax.plot_surface(x,y,-phi[x,y],cmap=cm.Accent,linewidth=0.0, antialiased=True) # phi is reversed just for better view
+ax.contour(x,y,phi[x,y],0)
+#ax.view_init(-80,35)
+ax2 = plt.figure(2)
+#ax2.imshow(img, extent = [0,255])
+"""
+
+#plt.figure(1)
+#plt.imshow(img, cmap = 'gray_r')
+#plt.contour(x,y,phi[x,y],0)
+#imshow(img)
+#plt.show()
+potential = 2
+if potential == 1:
+    potentialFunction = 'single-well'
+elif potential == 2:
+    potentialFunction = 'double-well'
+else:
+    potentialFunction = 'double-well'
+for i in range(iter_outer):
+    ph = phi
+    phi = drlse_edge(ph, g, lamda, mu, alfa, epsilon,timestep,iter_inner, potentialFunction)
+
+alfa = 0
+iter_refine = 10
+phi = drlse_edge(phi, g, lamda, mu, alfa, epsilon,timestep,iter_refine, potentialFunction)
+finalLSF = phi
+
+#ipdb.set_trace()
+#plt.imshow(img,interpolation='none', extent=(0, 59, 0, 77))
+#ax.plot_surface(x,y,-finalLSF[x,y],cmap=cm.Accent,linewidth=0.0, antialiased=True)
+#print(phi[:,22])
+
+fig, (ax1) = plt.subplots(nrows = 1,figsize = (3,5))
+ax1.imshow(img, cmap='gray', aspect='auto', interpolation='nearest')
+ax1.contour(y,x,finalLSF[x,y],0, colors = 'r', linewidths = 4)
+plt.show()
+#imshow(G)
+#print(img.shape)
+#imshow(imresize(img, (200,200)))
